f85c83338f761a8ba89395175fd970d333e56b89
Fender123 <pal.fender+git@gmail.com>
2016-03-01 20:11:29 +0100

    first database integration

--- routes/backend.js
+++ routes/backend.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var pageSettings = require('../lib/pageSettings.js');
 
 var backendPartials = {
     layout: 'backend/layout'
 };
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('backend/index.hjs', {
-    partials: backendPartials
-  });
+router.get('/', function (req, res, next) {
+    pageSettings.get().then(function(settings){
+        res.render('backend/index.hjs', {
+            tmp: JSON.stringify(settings),
+            partials: backendPartials
+        });
+    });
 });
 
 module.exports = router;
